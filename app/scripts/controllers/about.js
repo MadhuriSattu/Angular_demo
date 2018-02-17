b69c44e367b42654637bcc5fc4ad6e1e168ddb0e
Madhuri Sattu <madhuri.sattu@gmail.com>
2018-02-17 21:39:25 +0530

    added few functionality

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -9,7 +9,7 @@ define(['angular'], function (angular) {
    * Controller of the angularDemoApp
    */
   angular.module('angularDemoApp.controllers.AboutCtrl', [])
-    .controller('AboutCtrl',['$scope',function($scope){
+    .controller('AboutCtrl',['$scope','$http',function($scope,$http){
       $scope.ABC='hello';
       $scope.ASADAD="sasasas";
       $scope.arr=[
@@ -28,6 +28,58 @@ define(['angular'], function (angular) {
       ];
       $scope.abc=[];
       $scope.someValue="hellooo";
+      $scope.count=0;
+      $scope.counterFunction=function(){
+        $scope.count++;
+      };
+      $scope.selectFn=function(data){
+        alert(data);
+        //alert($scope.selectValue);
+      };
+
+      $scope.submitFn=function(data){
+        alert(data);
+      };
+
+     
+$scope.Employee=[{name:"madhuri",age:27,dob:"1483209000",salary:3000},
+{name:"mons",age:27,dob:"1483209000",salary:4000},
+{name:"raju",age:22,dob:"1483209000",salary:7000},
+];
+
+var obj1=[{name:"madhuri",age:27,dob:"1483209000",salary:3000},{name:"mons",age:27,dob:"1483209000",salary:4000}];
+for(var key in obj1){
+console.log(obj1.hasOwnProperty(key));
+}
+
+$scope.apiCallFn=function(){
+  console.log("in Funciton");
+  $http.get('https://reqres.in/api/users/2',{}).then(
+    function(response){
+      //success
+      console.log(response.data);
+    },
+    function(response){
+
+    //error
+    }
+  );
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }]);
 });
